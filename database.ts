1a0d8ed2a81b308561b31e9a4ff5c0f6fcc9dffa
ElmiriYounes <elmiri.younes@hotmail.com>
2022-11-10 13:23:28 +0100

    reset branch

--- database.ts
+++
@@ -1,9 +0,0 @@
-import mongoose, { Mongoose } from "mongoose";
-
-const uri: string = require('./config/keys').mongoURI;
-const dbName: string = require('./config/keys').mongoDbName;
-
-mongoose
-  .connect(uri, {dbName: dbName})
-  .then((db: Mongoose) => console.log("Database connected"))
-  .catch((err: unknown) => console.error(err));
