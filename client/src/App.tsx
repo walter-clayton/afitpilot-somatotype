8b1445d04fd83b8e1fdec0bbe9f1cf8f8de349b9
Bhama <bhamaguruswami@gmail.com>
2022-11-15 13:41:33 +0100

    merged with arnold app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,36 @@
-import './App.css';
 import Landing from './components/Landing';
+import './App.css';
+import Button from '@mui/material/Button';
+import ResponsiveAppBar from './components/AppNavBar';
+import { useState } from 'react';
+import Profile from './Profile';
 
 function App() {
+  const [currentPageIndex, setCurrentPageIndex] = useState(0); 
+
+  const pageHandler = (currentPageIndex:any) => {
+    setCurrentPageIndex(currentPageIndex);
+  }
+
   return (
+    
     <div>
-      <Landing/>
+            <Landing/>
+
+      <ResponsiveAppBar changeCurrentPage={pageHandler}/>
+      
+      {currentPageIndex === 0 ? 
+        <div>
+          <Button variant="contained">Hello World</Button>
+          <h1>Hello</h1>
+        </div> :
+        null 
+      }
+
+      {currentPageIndex === 1 ?
+        <Profile/> :
+        null 
+      }
     </div>
   );
 }
