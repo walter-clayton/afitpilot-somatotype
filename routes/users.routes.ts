4c871bfbbf63bc1ca4193e5ba4ef08a477f817c4
ElmiriYounes <elmiri.younes@hotmail.com>
2022-11-16 23:24:57 +0100

    send email to reset password

--- routes/users.routes.ts
+++ routes/users.routes.ts
@@ -5,20 +5,33 @@ const verifyToken = require("../middlewares/verifyToken.middleware");
 const verifyKey = require("../middlewares/verifyKey.middleware");
 const verifyUserRegister = require("../middlewares/verifyUserRegister.middleware");
 const verifyFields = require("../middlewares/verifyFieldsRegister.middleware");
-const verifyFieldsUpdateEmailAndUsername = require('../middlewares/verifyFieldsUpdateEmailUsername.middleware')
-const verifyFieldsPassword = require('../middlewares/verifyFieldsPassword.middleware')
+const verifyFieldsUpdateEmailAndUsername = require("../middlewares/verifyFieldsUpdateEmailUsername.middleware");
+const verifyFieldsPassword = require("../middlewares/verifyFieldsPassword.middleware");
+const verifyEmailResetPass = require("../middlewares/verifyEmailResetPass.middleware");
 const {
   register,
   getUser,
   deleteUser,
   updateEmailAndUsername,
-  updatePassword
+  updatePassword,
+  sendResetEmail,
 } = require("../controllers/users.controller");
 
 router.get("/getUser", verifyToken, getUser);
 router.post("/register", verifyKey, verifyFields, verifyUserRegister, register);
 router.delete("/deleteUser", verifyToken, deleteUser);
-router.post("/updateEmailAndUsername", verifyToken, verifyFieldsUpdateEmailAndUsername, updateEmailAndUsername);
-router.post('/updatePassword', verifyToken, verifyFieldsPassword, updatePassword)
+router.post(
+  "/updateEmailAndUsername",
+  verifyToken,
+  verifyFieldsUpdateEmailAndUsername,
+  updateEmailAndUsername
+);
+router.post(
+  "/updatePassword",
+  verifyToken,
+  verifyFieldsPassword,
+  updatePassword
+);
+router.post("/resetPassword", verifyKey, verifyEmailResetPass, sendResetEmail);
 
 module.exports = router;
