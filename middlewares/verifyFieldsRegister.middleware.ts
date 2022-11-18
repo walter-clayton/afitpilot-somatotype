e1f83baaac00ded65b720ad5130cd819e920839f
ElmiriYounes <elmiri.younes@hotmail.com>
2022-11-18 14:57:24 +0100

    register with randomly pass, update email and update pass

--- middlewares/verifyFieldsRegister.middleware.ts
+++ middlewares/verifyFieldsRegister.middleware.ts
@@ -13,16 +13,7 @@ interface IReqBody {
  * @returns Boolean
  */
 const isRequired = (reqBody: IReqBody): boolean => {
-  return (
-    !reqBody.email ||
-    !reqBody.username ||
-    !reqBody.password ||
-    !reqBody.confirmPassword ||
-    reqBody.email === "" ||
-    reqBody.username === "" ||
-    reqBody.password === "" ||
-    reqBody.confirmPassword === ""
-  );
+  return !reqBody.email || reqBody.email === "";
 };
 
 /**
@@ -40,39 +31,6 @@ const isEmailValid = (email: string): boolean => {
 };
 
 /**
- *
- * @param username String
- * @returns Boolean
- */
-const isUsernameValid = (username: string): boolean => {
-  const isValid: boolean = username.match(/^[a-zA-Z0-9]+$/) !== null;
-
-  return isValid;
-};
-
-/**
- *
- * @param pwd String
- * @param confirmPwd String
- * @returns boolean
- */
-const matchPassword = (pwd: string, confirmPwd: string): boolean => {
-  return pwd === confirmPwd;
-};
-
-/**
- * password must contains at least: 6 characters, 1 lowercase letter, 1 uppercase letter, 1 symbol as !@#$%^&* and 1 number
- * @param pwd String
- * @returns Boolean
- */
-const isPasswordValid = (pwd: string): boolean => {
-  const isValid: boolean =
-    pwd.match(/^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{6,20}$/) !==
-    null;
-  return isValid;
-};
-
-/**
  * Middleware
  */
 const isFieldsValid = (
@@ -80,32 +38,16 @@ const isFieldsValid = (
   res: Response,
   next: NextFunction
 ): express.Response<any, Record<string, any>> | undefined => {
-  const { email, username, password, confirmPassword }: IReqBody = req.body;
+  const { email }: IReqBody = req.body;
 
   if (isRequired(req.body))
     return res.status(403).send({
-      message: "Email ,username, password and confirmPassword are required.",
+      message: "Email is required on req.body.",
     });
 
   if (!isEmailValid(email))
     return res.status(403).send({
       message: "Invalid email.",
-    });
-
-  if (!isUsernameValid(username))
-    return res.status(403).send({
-      message: "Invalid username.",
-    });
-
-  if (!matchPassword(password, confirmPassword))
-    return res.status(403).send({
-      message: "mismatch password.",
-    });
-
-  if (!isPasswordValid(password))
-    return res.status(403).send({
-      message:
-        "Invalid password: password must contains at least: 6 characters, 1 lowercase letter, 1 uppercase letter, 1 symbol as !@#$%^&* and 1 number.",
     });
 
   next();
--- routes/users.routes.ts
+++ routes/users.routes.ts
@@ -3,38 +3,39 @@ import { Router } from "express";
 const router = Router();
 const verifyToken = require("../middlewares/verifyToken.middleware");
 const verifyKey = require("../middlewares/verifyKey.middleware");
-const verifyUserRegister = require("../middlewares/verifyUserRegister.middleware");
+const verifyUser = require("../middlewares/verifyUser.middleware");
+const verifyEmail = require('../middlewares/verifyEmail')
 const verifyFields = require("../middlewares/verifyFieldsRegister.middleware");
 const verifyFieldsUpdateEmailAndUsername = require("../middlewares/verifyFieldsUpdateEmailUsername.middleware");
 const verifyFieldsPassword = require("../middlewares/verifyFieldsPassword.middleware");
 const verifyEmailResetPass = require("../middlewares/verifyEmailResetPass.middleware");
-const verifyFieldsResetPass = require('../middlewares/verifyFieldsResetPass.middleware')
+const verifyFieldsResetPass = require("../middlewares/verifyFieldsResetPass.middleware");
 const {
   register,
   getUser,
   deleteUser,
-  updateEmailAndUsername,
+  updateEmail,
   updatePassword,
   sendResetEmail,
-  resetPassword
+  resetPassword,
 } = require("../controllers/users.controller");
 
 router.get("/getUser", verifyToken, getUser);
-router.post("/register", verifyKey, verifyFields, verifyUserRegister, register);
+router.post("/register", verifyKey, verifyFields, verifyUser, register);
 router.delete("/deleteUser", verifyToken, deleteUser);
-router.post(
-  "/updateEmailAndUsername",
-  verifyToken,
-  verifyFieldsUpdateEmailAndUsername,
-  updateEmailAndUsername
-);
+router.post("/updateEmail", verifyToken, verifyEmail, updateEmail);
 router.post(
   "/updatePassword",
   verifyToken,
-  verifyFieldsPassword,
-  updatePassword
+  sendResetEmail
 );
+// router.post(
+//   "/updatePassword",
+//   verifyToken,
+//   verifyFieldsPassword,
+//   updatePassword
+// );
 router.post("/forgotPassword", verifyKey, verifyEmailResetPass, sendResetEmail);
-router.post("/resetPassword", verifyKey, verifyFieldsResetPass, verifyToken, resetPassword);
+// router.post("/resetPassword", verifyKey, verifyFieldsResetPass, verifyToken, resetPassword);
 
 module.exports = router;
