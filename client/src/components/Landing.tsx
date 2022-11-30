798947f43c2d527d10fb78d00d9976633551c893
Walter Clayton <walter.h.clayton@gmail.com>
2022-11-30 09:58:58 +0100

    added some spelling

--- client/src/components/Landing.tsx
+++ client/src/components/Landing.tsx
@@ -7,7 +7,7 @@ import FilledInput from "@mui/material/FilledInput";
 import InputAdornment from "@mui/material/InputAdornment";
 import FormHelperText from "@mui/material/FormHelperText";
 import FormControl from "@mui/material/FormControl";
-import Typography from '@mui/material/Typography';
+import Typography from "@mui/material/Typography";
 import {
   AddPoint,
   calculateSomatotype,
@@ -70,49 +70,77 @@ const Landing: FC<ILanding> = (props) => {
     }));
   }, []);
   //Categorise the user from their submitted results
-  const getSomatotypeType = (endomorphy: number | undefined, mesomorphy: number | undefined, ectomorphy: number | undefined) => {
-    if ((endomorphy! > mesomorphy! && ectomorphy) && (mesomorphy! === ectomorphy)) {
-      return "You are Balanced endomorph Type"
+  const getSomatotypeType = (
+    endomorphy: number | undefined,
+    mesomorphy: number | undefined,
+    ectomorphy: number | undefined
+  ) => {
+    if (endomorphy! > mesomorphy! && ectomorphy && mesomorphy! === ectomorphy) {
+      return "You are a Balanced Endomorph Type";
+    } else if (
+      endomorphy! > mesomorphy! &&
+      ectomorphy &&
+      mesomorphy! > ectomorphy
+    ) {
+      return "You are a Mesomorphic Endomorph Type";
+    } else if (
+      endomorphy! === mesomorphy! &&
+      mesomorphy! &&
+      endomorphy > ectomorphy!
+    ) {
+      return "You are a Mesomorph-Endomorph Type";
+    } else if (
+      mesomorphy! > endomorphy! &&
+      ectomorphy &&
+      endomorphy! > ectomorphy
+    ) {
+      return "You are an Endomorphic Mesomorph Type";
+    } else if (
+      mesomorphy! > endomorphy! &&
+      ectomorphy &&
+      endomorphy! <= ectomorphy
+    ) {
+      return "You are a Balanced Mesomorph Type";
+    } else if (
+      mesomorphy! > endomorphy! &&
+      ectomorphy &&
+      ectomorphy! > endomorphy!
+    ) {
+      return "You are an Ectomorphic Mesomorph Type";
+    } else if (mesomorphy! === ectomorphy && ectomorphy! > endomorphy!) {
+      return "You are a Mesomorph-Ectomorph Type";
+    } else if (
+      ectomorphy! > mesomorphy! &&
+      endomorphy &&
+      mesomorphy! >= endomorphy!
+    ) {
+      return "You are a Mesomorphic-Ectomorph Type";
+    } else if (
+      ectomorphy! > mesomorphy! &&
+      endomorphy &&
+      mesomorphy! === endomorphy!
+    ) {
+      return "You are a Balanced Ectomorph Type";
+    } else if (
+      ectomorphy! > mesomorphy! &&
+      endomorphy &&
+      endomorphy! > mesomorphy!
+    ) {
+      return "You are an Endomorphic Ectomorph Type";
+    } else if (endomorphy! === ectomorphy! && endomorphy! >= mesomorphy!) {
+      return "You are an Endomorph-Ectomorph Type";
+    } else if (endomorphy! >= ectomorphy! && ectomorphy! >= mesomorphy!) {
+      return "You are an Ectomorphic Endomorph Type";
+    } else {
+      return "You are a Central Type";
     }
-    else if ((endomorphy! > mesomorphy! && ectomorphy) && (mesomorphy! > ectomorphy)) {
-      return "You are Mesomorphic endomorph Type"
-    }
-    else if ((endomorphy! === mesomorphy!) && (mesomorphy! && endomorphy > ectomorphy!)) {
-      return "You are Mesomorph-endomorph Type"
-    }
-    else if ((mesomorphy! > endomorphy! && ectomorphy) && (endomorphy! > ectomorphy)) {
-      return "You are Endomorphic mesomorph Type"
-    }
-    else if ((mesomorphy! > endomorphy! && ectomorphy) && (endomorphy! <= ectomorphy)) {
-      return "You are Balanced mesomorph Type"
-    }
-    else if ((mesomorphy! > endomorphy! && ectomorphy) && (ectomorphy! > endomorphy!)) {
-      return "You are Ectomorphic mesomorph Type"
-    }
-    else if ((mesomorphy! === ectomorphy) && (ectomorphy! > endomorphy!)) {
-      return "You are Mesomorph-ectomorph Type"
-    }
-    else if ((ectomorphy! > mesomorphy! && endomorphy) && (mesomorphy! >= endomorphy!)) {
-      return "You are Mesomorphic-ectomorph Type"
-    }
-    else if ((ectomorphy! > mesomorphy! && endomorphy) && (mesomorphy! >= endomorphy!)) {
-      return "You are Balanced ectomorph Type"
-    }
-    else if ((ectomorphy! > mesomorphy! && endomorphy) && (endomorphy! > mesomorphy!)) {
-      return "You are Endomorphic ectomorph Type"
-    }
-    else if ((endomorphy! === ectomorphy!) && (endomorphy! >= mesomorphy!)) {
-      return "You are Endomorph-ectomorph Type"
-    }
-    else if ((endomorphy! >= ectomorphy!) && (ectomorphy! >= mesomorphy!)) {
-      return "You are Ectomorphic endomorph Type"
-    }
-    else {
-      return "You are central"
-    }
-  }
-  const typeResult = getSomatotypeType(somatotype?.endomorphy, somatotype?.mesomorphy, somatotype?.ectomorphy);
-  console.log(typeResult)
+  };
+  const typeResult = getSomatotypeType(
+    somatotype?.endomorphy,
+    somatotype?.mesomorphy,
+    somatotype?.ectomorphy
+  );
+  console.log(typeResult);
 
   const saveDatas = async () => {
     try {
@@ -226,7 +254,11 @@ const Landing: FC<ILanding> = (props) => {
                 const somatotypeResults = calculateSomatotype(anthropometric!);
 
                 let pointsResultsArray: IPoints[] = [];
-                const point = AddPoint(somatotypeResults[0], somatotypeResults[1], somatotypeResults[2]);
+                const point = AddPoint(
+                  somatotypeResults[0],
+                  somatotypeResults[1],
+                  somatotypeResults[2]
+                );
                 pointsResultsArray.push(point);
                 setPointsArray(pointsResultsArray);
 
