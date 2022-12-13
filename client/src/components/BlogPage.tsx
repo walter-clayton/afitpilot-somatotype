5a3deee278efbd9cc6918c0066f7ebab86d9fff8
ArnaudDalcq <aranaud.dalcq@gmail.com>
2022-12-13 16:49:09 +0100

    Add blog pagination

--- client/src/components/BlogPage.tsx
+++ client/src/components/BlogPage.tsx
@@ -1,4 +1,4 @@
-import { Grid } from "@mui/material";
+import { Grid, useMediaQuery } from "@mui/material";
 import { Box } from "@mui/system";
 import React, { FC, useEffect, useState } from "react";
 import BlogArticlePage from "./BlogArticlePage";
@@ -10,7 +10,9 @@ import {
   IBlogImage,
   IBlogTextWithImage,
 } from "./BlogContent";
-import CounterShare from './CTA/CounterShare'
+import CounterShare from "./CTA/CounterShare";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
 
 const createBlogCard = (
   BlogTitle: string,
@@ -49,7 +51,18 @@ export interface IBlogCardInfos {
 }
 
 const BlogPage = () => {
+  const xxs = useMediaQuery("(max-width:420px)");
+  const xxxs = useMediaQuery("(max-width:320px)");
+  const large = useMediaQuery("(min-width:1200px)");
   const [blogCards, setBlogCards] = useState<IBlogCardInfos[]>([]);
+
+  const blogArticlesPerPage: number = 6;
+  const [page, setPage] = useState(0);
+
+  const handleChangePage = (newPage: number) => {
+    window.scrollTo(0, 0);
+    setPage(newPage - 1);
+  };
 
   useEffect(() => {
     setBlogCards([]);
@@ -75,18 +88,70 @@ const BlogPage = () => {
   return (
     <>
       <Grid container px={4}>
-        {blogCards.map((blogCard, index) => (
-          <Grid item xs={12} sm={12} md={6} lg={4} paddingTop={4} key={index}>
-            <Grid container alignContent={"center"} justifyContent={"center"}>
-              <BlogCard index={index} blogCard={blogCard} />
+        {blogCards
+          .slice(
+            page * blogArticlesPerPage,
+            page * blogArticlesPerPage + blogArticlesPerPage
+          )
+          .map((blogCard, index) => (
+            <Grid item xs={12} sm={12} md={6} lg={4} paddingTop={4} key={index}>
+              <Grid container alignContent={"center"} justifyContent={"center"}>
+                <BlogCard
+                  index={page * blogArticlesPerPage + index}
+                  blogCard={blogCard}
+                />
+              </Grid>
             </Grid>
-          </Grid>
-        ))}
+          ))}
+        <Grid
+          item
+          xs={12}
+          paddingTop={4}
+          key={"pagination"}
+          justifySelf={"center"}
+        >
+          <Stack alignItems="center">
+            <Pagination
+              count={Math.ceil(blogCards.length / blogArticlesPerPage)}
+              variant="outlined"
+              shape="circular"
+              onChange={(e, value) => {
+                handleChangePage(value);
+              }}
+              size={xxs ? "small" : large ? "large" : "medium"}
+              sx={{
+                "& .Mui-selected": { backgroundColor: "#d1d1d1" },
+                "& .MuiPaginationItem-root.Mui-selected": {
+                  backgroundColor: "#d1d1d1",
+                },
+                "& .MuiPaginationItem-root.Mui-selected:hover": {
+                  backgroundColor: "#d1d1d1",
+                },
+                "& .MuiPaginationItem-root": {
+                  minWidth: xxxs
+                    ? "20px"
+                    : xxs
+                    ? "26px"
+                    : large
+                    ? "40px"
+                    : "32px",
+                  height: xxxs
+                    ? "20px"
+                    : xxs
+                    ? "26px"
+                    : large
+                    ? "40px"
+                    : "32px",
+                  fontSize: xxxs ? "70%" : large ? "108%" : "90%",
+                },
+              }}
+            />
+          </Stack>
+        </Grid>
       </Grid>
       <CounterShare />
     </>
   );
-
 };
 
 export default BlogPage;
