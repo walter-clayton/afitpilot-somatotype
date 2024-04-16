24011fea386bf4b0a563b453f90ba3ac1d75d3a2
Yves Bolong <yve17@hotmail.be>
2024-04-16 15:48:00 +0200

    first staging push

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -7,11 +7,28 @@ import ReactGA from "react-ga4";
 const GA_MEASUREMENT_ID = "G-VE6XKWVTFG";
 ReactGA.initialize(GA_MEASUREMENT_ID);
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(<App />);
+if (process.env.REACT_APP_ENVIRONMENT === "staging") {
+  const username = prompt("Enter username");
+  const password = prompt("Enter password");
 
+  const expectedUsername = "afitpilot";
+  const expectedPassword = "staging";
+
+  if (username !== expectedUsername || password !== expectedPassword) {
+    alert("Invalid credentials");
+    window.close();
+  } else {
+    const root = ReactDOM.createRoot(
+      document.getElementById("root") as HTMLElement
+    );
+    root.render(<App />);
+  }
+} else {
+  const root = ReactDOM.createRoot(
+    document.getElementById("root") as HTMLElement
+  );
+  root.render(<App />);
+}
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
