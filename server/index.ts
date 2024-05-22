be14337d7476dba4896e438ec36bd214669b7bb6
matthieuGravy <matthieugravy@gmail.com>
2024-05-22 12:52:22 +0200

    AF-69_package.json-review

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,42 @@
-import { Express } from "express";
 require("./database");
 
-const app: Express = require("./server");
+import express, { Express } from "express";
+const cors = require("cors");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const routesUsers = require("./routes/users.routes");
+const routesAuth = require("./routes/auth.routes");
+const routesCompare = require("./routes/compare.routes");
+import workoutRoutes from "./routes/workout.routes";
+import rpeRoutes from "./routes/rpe.routes";
+//import rpeDashboardRoutes from "./routes/rpedashboard.routes";
+
+// Initialization
+const app: Express = express();
+const corsOptions = {
+  origin: ["http://localhost:3000"],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`[server]: Server is running at ${PORT}`);
+});
+
+dotenv.config();
+
+// Routes
+// app.use(routesUsers);
+app.use("/users", routesUsers);
+app.use("/auth", routesAuth);
+app.use("/compare", routesCompare);
+app.use("/workouts", workoutRoutes);
+app.use("/rpe", rpeRoutes);
+//app.use("/dashboard", rpeDashboardRoutes);
+
+module.exports = app;
